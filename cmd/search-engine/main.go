938afeb357d502e36e8ed6b39ad40d50fa9a6637
David Ulloa <dulloa6310@gmail.com>
2023-11-24 19:19:10 -0500

    Adjust documents to index

--- cmd/search-engine/main.go
+++ cmd/search-engine/main.go
@@ -10,12 +10,17 @@ import (
 func main() {
 
 	cluster := gocql.NewCluster("127.0.0.1")
-    cluster.Keyspace = "indexing"
+    cluster.Keyspace = "tfidf_keyspace"
     session, err := cluster.CreateSession()
     if err != nil {
         log.Fatal(err)
     }
     defer session.Close()
 
-	crawl.IndexDocument(session, "https://apnews.com/")
+	crawl.IndexDocument(session, "https://en.wikipedia.org/wiki/United_States")
+	crawl.IndexDocument(session, "https://en.wikipedia.org/wiki/Russia")
+	crawl.IndexDocument(session, "https://en.wikipedia.org/wiki/Pineapple")
+	crawl.IndexDocument(session, "https://en.wikipedia.org/wiki/Basketball")
+	crawl.IndexDocument(session, "https://en.wikipedia.org/wiki/Botany")
+	crawl.IndexDocument(session, "https://en.wikipedia.org/wiki/Quantum_mechanics")
 }
